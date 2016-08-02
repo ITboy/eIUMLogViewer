048b632000d44b64a7be8967b52526f3ddb39717
Zhu Tengfei <tengfei.zhu@hpe.com>
2016-08-02 18:05:07 +0800

    update daily

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 var path=require("path");
 var fs = require("fs");
+var spawn = require('child_process').spawn;
+
 var eIUMLogViewer = require("eIUMLogViewerComponent");
+
+var server = "snap@15.114.119.56";
+var filename = "/var/opt/SIU_snap/log/ocs.log";
 
 exports.index = function(req, res) {
   var logContent = [];
+  var tail = spawn("ssh", [server, "cat", filename]);
 
   fs.createReadStream(path.join(__dirname, "../ocs.log"))
+  //tail.stdout
     .pipe(eIUMLogViewer.spiltLine())
     .pipe(eIUMLogViewer.reAlignLine())
     .pipe(eIUMLogViewer.parseLogLine())
