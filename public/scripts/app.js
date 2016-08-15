a0ffb9c8ddcfdfcf1ad562a8b6118a88115f0801
tengfei.zhu <prepper@126.com>
2016-08-15 22:13:24 +0800

    add bower

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,7 @@
-$(function(){
-  var socket = io();
+  //var socket = io();
 
   //socket.on('connection', function(socket))
-});
+  var app = angular.module('eIUMLogViewerApp', []);
+  app.controller('getLogObjController', function($scope){
+    $scope.name = 'leo';
+  });
